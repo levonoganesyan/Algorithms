d212749751fe8370070e6aba5407f186555c00b2
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-05-05 18:14:57 +0400

    Changing try catch block

--- CodePreprocessor/Source.cpp
+++ CodePreprocessor/Source.cpp
@@ -1,26 +1,23 @@
 #include<iostream>
 #include"FileProcessor.h"
 
-int wmain(int argc, wchar_t** argv)
+int wmain(int argc, wchar_t** argv) try
 {
-    try
+    std::wstring file_path = argv[1];
+    std::vector<std::wstring> folder_paths;
+    for (int i = 2; i < argc; ++i)
     {
-        std::wstring file_path = argv[1];
-        std::vector<std::wstring> folder_paths;
-        for (int i = 2; i < argc; ++i)
-        {
-            folder_paths.push_back(argv[i]);
-        }
-        FileProcessor file_processor(file_path, folder_paths);
-        file_processor.Process();
-        std::cout << "Code preprocessing is "
-                  << "successfully finished!"
-                  << std::endl;
+        folder_paths.push_back(argv[i]);
     }
-    catch (std::exception ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
+    FileProcessor file_processor(file_path, folder_paths);
+    file_processor.Process();
+    std::cout << "Code preprocessing is "
+                << "successfully finished!"
+                << std::endl;
+}
+catch (const std::exception& ex)
+{
+    std::cout << ex.what() << std::endl;
 }
 
 
