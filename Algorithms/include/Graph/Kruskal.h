0350859556bedbdd2372883b0535562d0ac1c59c
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-05-13 00:25:37 +0400

    Added Kruskal implementation

--- Algorithms/include/Graph/Kruskal.h
+++ Algorithms/include/Graph/Kruskal.h
@@ -7,13 +7,21 @@ namespace algo
     {
     private:
         algo::DSU m_dsu;
+        Graph::WeightType m_cost;
+        std::vector<Graph::Edge> m_spanning_tree;
+        
         using ConnectionList = Graph::ConnectionList;
         using ConnectionMatrix = Graph::ConnectionMatrix;
         using ListOfEdges = Graph::ListOfEdges;
+
+
     public:
         Kruskal(const ConnectionList & graph);
         Kruskal(const ConnectionMatrix& graph);
         Kruskal(ListOfEdges graph);
+
+        std::vector<Graph::Edge> GetTree() const;
+        Graph::WeightType GetCost() const;
 
     };
 }
--- Algorithms/src/Graph/Kruskal.cpp
+++ Algorithms/src/Graph/Kruskal.cpp
@@ -13,7 +13,31 @@ namespace algo
     Kruskal::Kruskal(ListOfEdges graph)
         : m_dsu(Graph::GetSize(graph))
     {
-        std::set<Graph::Edge> all_edges;
+        std::set<Graph::Edge> all_edges(graph.begin(), graph.end());
+        size_t number_of_edges = graph.size();
+        for (size_t i = 0; i < number_of_edges; ++i)
+        {
+            Graph::Edge e = *all_edges.begin();
+            all_edges.erase(all_edges.begin());
+            Graph::VertexType from = e.from;
+            Graph::VertexType to = e.to;
+            Graph::WeightType weight = e.weight;
+
+            if (m_dsu.GetParent(from) != m_dsu.GetParent(to))
+            {
+                m_cost + weight;
+                m_spanning_tree.push_back(e);
+                m_dsu.Union(from, to);
+            }
+        }
+    }
+    std::vector<Graph::Edge> Kruskal::GetTree() const
+    {
+        return m_spanning_tree;
+    }
+    Graph::WeightType Kruskal::GetCost() const
+    {
+        return m_cost;
     }
 }
 
