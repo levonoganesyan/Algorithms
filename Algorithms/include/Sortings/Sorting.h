bed44f70554623df9daf5efa190a2e105a191e0f
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-08-04 22:48:10 +0400

    Start BubbleSort

--- Algorithms/include/Sortings/Sorting.h
+++ Algorithms/include/Sortings/Sorting.h
@@ -1,9 +1,33 @@
 #pragma once
+#include<iterator>
 
-
-class Sorting
+namespace algo
 {
-public:
+	//template<typename Iter>
+	//void sort(const Iter& first, const Iter& second);
+
 	template<typename Iter>
-	virtual void sort(const Iter& first, const Iter& second) = 0;
-};
+	void BubbleSort(const Iter& first, const Iter& last)
+	{
+		Iter temp = first;
+		bool need_to_continue = true;
+		while (need_to_continue)
+		{
+			need_to_continue = false;
+			while (std::next(first) != last)
+			{
+				need_to_continue = true;
+				Iter& next = std::next(first);
+				if (*next < *first)
+				{
+					std::swap(*next, *first);
+				}
+			}
+			last = std::prev(last);
+		}
+	}
+
+
+
+
+}
