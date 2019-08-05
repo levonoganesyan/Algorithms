34cd7d459643a0105cd7181679c0aaa7fc04ce1f
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-08-05 22:55:40 +0400

    Test start

---
+++ AlgorithmTest/SortingTest.cpp
@@ -0,0 +1,23 @@
+#include"pch.h"
+
+#include"Sortings/Sorting.h"
+#include<vector>
+#include<random>
+#include<ctime>
+
+std::vector<int> getRandomVector(int n, int min = -1000, int max = 1000)
+{
+	std::vector<int> random_vector(n);
+	std::mt19937 rand_function(time(0));
+	
+	for (auto& elem : random_vector)
+	{
+	}
+}
+
+TEST(SortingTest, SortingTest)
+{
+	{
+		
+	}
+}
