02c6b63af46804331869d4462763f312445cb389
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-08-06 22:59:22 +0400

    ContinuingTest

--- AlgorithmTest/SortingTest.cpp
+++ AlgorithmTest/SortingTest.cpp
@@ -9,15 +9,20 @@ std::vector<int> getRandomVector(int n, int min = -1000, int max = 1000)
 {
 	std::vector<int> random_vector(n);
 	std::mt19937 rand_function(time(0));
-	
+	std::uniform_int_distribution<int> uid(min, max);
 	for (auto& elem : random_vector)
 	{
+		elem = uid(rand_function);
 	}
+	return random_vector;
 }
+
 
 TEST(SortingTest, SortingTest)
 {
 	{
-		
+		auto first = getRandomVector(1000);
+		auto second = first;
+
 	}
 }
