f14844afb32d9c47498c38dd8042a41b6bcea3de
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-08-10 23:27:17 +0400

    Bubble sort finished

--- Algorithms/include/Sortings/Sorting.h
+++ Algorithms/include/Sortings/Sorting.h
@@ -9,15 +9,10 @@ namespace algo
 	template<typename Iter>
 	void BubbleSort(Iter first, Iter last)
 	{
-		for (Iter i = first; i != last; std::next(i))
-		{
-			for (Iter j = first; j < i; std::next(j))
+		for (Iter i = first; i != last; i = std::next(i))
+			for (Iter j = first; j < i; j = std::next(j))
 				if (*i < *j)
 					std::iter_swap(i, j);
-			for (Iter i = first; i != last; i++)
-				std::cout << *i << " ";
-			std::cout << std::endl;
-		}
 	}
 
 
