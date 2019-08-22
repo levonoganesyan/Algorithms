63c9df9f780b491b77195b2e0e1823f5367b7b30
Levon Oganesyan <levon.oganesyan.am@gmail.com>
2019-08-22 18:49:59 +0400

    Some changes + new filters

---
+++ Algorithms/src/Graph/LCA.cpp
@@ -0,0 +1,21 @@
+#include "Graph\LCA.h"
+
+void algo::LCA::dfs(const ConnectionList& graph, int vertex)
+{
+}
+
+algo::LCA::LCA(const Graph& graph)
+{
+}
+
+algo::LCA::LCA(const ConnectionList& graph)
+{
+}
+
+algo::LCA::LCA(const ConnectionMatrix& graph)
+{
+}
+
+algo::LCA::LCA(const ListOfEdges& graph)
+{
+}
