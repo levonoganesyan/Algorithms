a717903e850a70b3ee262092bc9a66aa8f949e9b
Levon Oganesyan <levon.oganesyan.am@gmail.com>
2019-08-22 11:40:44 +0400

    Added some test cases and fixed others

--- AlgorithmTest/DataStructuresTest.cpp
+++ AlgorithmTest/DataStructuresTest.cpp
@@ -23,6 +23,24 @@ int mul_on_vector(const std::vector<int>& vec, int i, int j)
 	}
 	return mul;
 }
+int min_on_vector(const std::vector<int>& vec, int i, int j)
+{
+	int g = vec[i++];
+	while (i <= j)
+	{
+		g = std::min(g, vec[i++]);
+	}
+	return g;
+}
+int max_on_vector(const std::vector<int>& vec, int i, int j)
+{
+	int g = vec[i++];
+	while (i <= j)
+	{
+		g = std::max(g, vec[i++]);
+	}
+	return g;
+}
 int gcd_on_vector(const std::vector<int>& vec, int i, int j)
 {
 	int g = vec[i++];
@@ -50,7 +68,7 @@ TEST(SegmentTree, DataStructures)
 		if (a > b)
 			std::swap(a, b);
 		EXPECT_EQ(tree.query(a, b), sum_on_vector(vec, a, b));
-		tree.update(a, vec[a + 1]);
+		tree.update(a, vec[a] + 1);
 	}
 	// product
 	for (int i = 0; i < 10; ++i)
@@ -64,7 +82,37 @@ TEST(SegmentTree, DataStructures)
 		if (a > b)
 			std::swap(a, b);
 		EXPECT_EQ(tree.query(a, b), mul_on_vector(vec, a, b));
-		tree.update(a, vec[a + 1]);
+		tree.update(a, vec[a] + 1);
+	}
+	// min
+	for (int i = 0; i < 10; ++i)
+	{
+		std::vector<int> vec = getRandomVector(1000, -1111, 1111);
+		const int& (*operation)(const int&, const int&) = std::min<int>;
+		algo::SegmentTree<int> tree(vec, operation, 1111);
+		tree.build_tree();
+		std::uniform_int_distribution<int> uid(0, vec.size() - 1);
+		int a = uid(rand_function);
+		int b = uid(rand_function);
+		if (a > b)
+			std::swap(a, b);
+		EXPECT_EQ(tree.query(a, b), min_on_vector(vec, a, b));
+		tree.update(a, vec[a] + 1);
+	}
+	// max
+	for (int i = 0; i < 10; ++i)
+	{
+		std::vector<int> vec = getRandomVector(1000, -1111, 1111);
+		const int& (*operation)(const int&, const int&) = std::max<int>;
+		algo::SegmentTree<int> tree(vec, operation, -1111);
+		tree.build_tree();
+		std::uniform_int_distribution<int> uid(0, vec.size() - 1);
+		int a = uid(rand_function);
+		int b = uid(rand_function);
+		if (a > b)
+			std::swap(a, b);
+		EXPECT_EQ(tree.query(a, b), max_on_vector(vec, a, b));
+		tree.update(a, vec[a] + 1);
 	}
 	// gcd
 	for (int i = 0; i < 10; ++i)
@@ -78,6 +126,6 @@ TEST(SegmentTree, DataStructures)
 		if (a > b)
 			std::swap(a, b);
 		EXPECT_EQ(tree.query(a, b), gcd_on_vector(vec, a, b));
-		tree.update(a, vec[a + 1]);
+		tree.update(a, vec[a] + 1);
 	}
 }
