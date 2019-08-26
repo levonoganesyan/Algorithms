c260259438689b3797551757bdd340969b98c306
Levon Oganesyan <levon.oganesyan.am@gmail.com>
2019-08-26 10:18:24 +0400

    Added Z-function

---
+++ Algorithms/include/Strings/Z-Function.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<string>
+#include<vector>
+
+namespace algo
+{
+	class Z_Function
+	{
+	private:
+		std::string m_str;
+		std::vector<int> m_z;
+		void calculate();
+	public:
+		Z_Function(const std::string& str);
+		std::vector<int> GetZ() const;
+	};
+}
---
+++ Algorithms/src/Strings/Z-Function.cpp
@@ -0,0 +1,37 @@
+#include<Strings/Z-Function.h>
+namespace algo
+{
+	void Z_Function::calculate()
+	{
+		for (int i = 1, l = 0, r = 0; i < m_str.size(); ++i)
+		{
+			if (i <= r)
+			{
+				m_z[i] = min(r - i + 1, m_z[i - l]);
+			}
+			while (i + m_z[i] < m_str.size() && m_str[i + m_z[i]] == m_str[m_z[i]])
+			{
+				++m_z[i];
+			}
+			if (i + m_z[i] - 1 > r)
+			{
+				l = i;
+				r = i + m_z[i] - 1;
+			}
+		}
+	}
+
+	Z_Function::Z_Function(const std::string& str)
+		: m_str(str)
+		, z(str.size())
+	{
+		calculate();
+	}
+
+	std::vector<int> Z_Function::GetZ() const
+	{
+		return m_z;
+	}
+
+
+}
