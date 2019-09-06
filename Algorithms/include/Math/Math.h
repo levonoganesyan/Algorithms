7f24cc4bae5f069c8c242449d7b0c34597a8aa49
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-09-07 01:54:29 +0400

    Added extended euclidean algorithm

--- Algorithms/include/Math/Math.h
+++ Algorithms/include/Math/Math.h
@@ -109,7 +109,19 @@ namespace algo
         }
         return primes;
     }
-
+    int extended_euclidean(int a, int b, int& x, int& y)
+    {
+        if (a == 0)
+        {
+            x = 0, y = 1;
+            return b;
+        }
+        int x1, y1;
+        int gcd = extended_euclidean(b % a, a, x1, y1);
+        x = y1 - b / a * x1;
+        y = x1;
+        return gcd;
+    }
 }
 
 
